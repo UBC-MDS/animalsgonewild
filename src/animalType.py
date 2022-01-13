bdda69ca15fe7790392a4e3a6ece436042cc43cc
morganrosenberg50 <morgan.ar.rosenberg@gmail.com>
2022-01-12 19:20:09 -0800

    Function 2 animalType
    Adding function 2 animalType empty with complete docstring

---
+++ src/animalType.py
@@ -0,0 +1,22 @@
+def animalType(species, text):
+    """    
+    Analyzes text based on average word length and returns an animal picture representing language complexity. 
+    
+    Parameters
+    ----------
+    species: str
+        name of the species determined from <INSERT FUNCTION NAME> or user determined
+    text: str
+        text to be analyzed
+    Returns
+    -------
+    jpg
+        an image representative of the species and the complexity of the text (more complex text will have 'wiser' looking animals)
+    
+    Example
+    --------
+    >>> animal_rep = animalType(FUNCTION NAME(text), text) 
+    >>> animal_rep = animalType("duck", text) 
+    >>> animal_rep(species, text)
+    reallySmartDuck.jpg
+    """
