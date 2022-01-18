581d4d2be3750539b164ca076cd24ede58cb02cc
Junrong Zhu <junrongz@student.ubc.ca>
2022-01-18 14:46:56 -0800

    Complete the function body

--- src/animalClassifier.py
+++ src/animalClassifier.py
@@ -1,6 +1,9 @@
+import string
+
+
 def animalClassifier(text):
-    """    
-    Count total characters in the input text and returns an animal type. 
+    """
+    Count total unique words in the input text and returns an animal type.
     
     Parameters
     ----------
@@ -11,10 +14,25 @@ def animalClassifier(text):
     -------
     str
         the corresponding animal species
-    
+        
     Example
     --------
-    >>> animalClassifier("what do you think?") 
+    >>> animalClassifier("what do you think?")
     "duck"
     """
+    animal_list = ["Duck", "Monkey", "Giraffe", "Whale"]
+    
+    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
+    unique_words = np.unique(np.array(str.split(cleaned_text)))
+    counts = len(unique_words)
+
+    if counts <= 20:
+        output = animal_list[0]
+    elif counts <= 80:
+        output = animal_list[1]
+    elif counts <= 160:
+        output = animal_list[2]
+    else:
+        output = animal_list[3]
+
     return output
