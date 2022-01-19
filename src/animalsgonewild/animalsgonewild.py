ec18ed0233a59461aa2e348ef917da7167fd54bc
Junrong Zhu <junrongz@student.ubc.ca>
2022-01-18 16:22:29 -0800

    Edit function1 based on PR feedback

--- src/animalsgonewild/animalsgonewild.py
+++ src/animalsgonewild/animalsgonewild.py
@@ -1,7 +1,9 @@
+import string
+import numpy as np
 
 def animalClassifier(text):
     """    
-    Count total characters in the input text and returns an animal type. 
+    Count proportion of unique words in the input text and returns an corresponding animal type. 
     
     Parameters
     ----------
@@ -18,7 +20,22 @@ def animalClassifier(text):
     >>> animalClassifier("what do you think?") 
     "duck"
     """
-    output = ''
+    animal_list = ["Duck", "Monkey", "Giraffe", "Whale"]
+    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
+    total_words = len(str.split(cleaned_text))
+    unique_words = np.unique(str.split(cleaned_text))
+    uni_counts = len(unique_words)
+
+
+    if uni_counts/total_words <= 0.4:
+        output = animal_list[0]
+    elif uni_counts/total_words <= 0.6:
+        output = animal_list[1]
+    elif uni_counts/total_words <= 0.8:
+        output = animal_list[2]
+    else:
+        output = animal_list[3]
+
     return output
 
 def animalType(species, text):
